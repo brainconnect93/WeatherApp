012823c6ee87cefa5ec239f19142eaf50746cb17
Afolabi Akorede <brainconnect0@gmail.com>
2024-07-25 11:52:55 +0100

    Create the ViewComponent for the weather app

--- Models/CityWeather.cs
+++ Models/CityWeather.cs
@@ -5,6 +5,6 @@
         public string? CityUniqueCode { get; set; }
         public string? CityName { get; set; }
         public DateTime? DateAndTime { get; set; }
-        public int TemperatureFehrenheit { get; set; }
+        public int TemperatureFahrenheit { get; set; }
     }
 }
---
+++ ViewComponents/CityViewComponents.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.ViewComponents
+{
+    public class CityViewComponents : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(CityWeather model)
+        {
+            ViewBag.CssByDegree = GetCssClassByFahrenheit(model.TemperatureFahrenheit);
+            return View(model);
+        }
+
+        // function: to get CSS background color based on the degree range
+
+        private string GetCssClassByFahrenheit(int TemperatureFahrenheit)
+        {
+            return TemperatureFahrenheit switch
+            {
+                (< 44) => "blue-back",
+                (>= 44) and (< 78) => "green-back",
+                (>= 75) => "orange-back"
+            };
+        }
+    }
+}
+
